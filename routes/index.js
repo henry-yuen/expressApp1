e30812dda40a4a6ef4c5c7326b25151207c216b8
Henry Yuen <Henry Yuen>
2017-01-20 12:21:56 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+    res.json({ message: 'API Initialized!' });
+});
+
+router.route('/test')
+	// retrieve all comments from the database
+	.get(function(req, res){
+		// look at Comment schema
+        res.json({ message: 'Test GET succeeded.' });
+	})
+	
+module.exports = router;
